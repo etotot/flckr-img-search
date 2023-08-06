70577d9df5fef5dfec9929e06bca05b3e67a2664
Andrey Marshak <andrey.marshak@etotot.xyz>
2023-08-06 19:40:48 +0300

    Add model structs

---
+++ image-search/ImageSearch/Shared/Model/Model.swift
@@ -0,0 +1,30 @@
+//
+//  Model.swift
+//  ImageSearch
+//
+//  Created by andrey.marshak on 06.08.2023.
+//
+// This file was generated from JSON Schema using quicktype, do not modify it directly.
+
+import Foundation
+
+// MARK: - Search
+struct Search: Codable {
+    let photos: Photos
+    let stat: String
+}
+
+// MARK: - Photos
+struct Photos: Codable {
+    let page, pages, perpage: Int
+    let photo: [Photo]
+    let total: Int
+}
+
+// MARK: - Photo
+struct Photo: Codable {
+    let farm: Int
+    let id: String
+    let isfamily, isfriend, ispublic: Int
+    let owner, secret, server, title: String
+}
